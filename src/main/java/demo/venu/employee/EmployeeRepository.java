a5a1ce3b70dd0c9bf89429f027c256ebcec01ba1
venu emmadi <emmadivenu2310@gmail.com>
2024-05-27 16:34:14 +0530

    Second commit

--- src/main/java/demo/venu/employee/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package demo.venu.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-    Employee findByName(String name);
-}
